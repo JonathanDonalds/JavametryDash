393d82145f1343eeec9d0db4a8677b65f07252d6
Jonathan <jdonalds100@gmail.com>
2024-05-20 13:17:30 -0400

    1:17 PM

--- src/WelcomeFrame.java
+++ src/WelcomeFrame.java
@@ -7,15 +7,11 @@ public class WelcomeFrame {
         JFrame frame = new JFrame("Welcome");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1920, 480);
-        frame.setLocationRelativeTo(null); // auto-centers frame in screen
+        frame.setLocationRelativeTo(null);
 
-        // create and add panel
         PANEL = new WelcomePanel(frame);
         frame.add(PANEL);
 
-        // display the frame
         frame.setVisible(true);
-
-        // no thread needed here since we aren't doing animation for this frame/panel
     }
 }
