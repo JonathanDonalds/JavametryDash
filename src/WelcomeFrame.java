8d343e5dc96a03a5892de2a94837a4affe69e40b
Jonathan <jdonalds100@gmail.com>
2024-05-21 13:43:48 -0400

    1:43 PM

--- src/WelcomeFrame.java
+++ src/WelcomeFrame.java
@@ -1,17 +1,27 @@
 import javax.swing.*;
 
-public class WelcomeFrame {
+public class WelcomeFrame implements Runnable {
     private final WelcomePanel PANEL;
 
     public WelcomeFrame() {
         JFrame frame = new JFrame("Welcome");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1920, 480);
+        frame.setSize(2560, 1600);
         frame.setLocationRelativeTo(null);
 
         PANEL = new WelcomePanel(frame);
         frame.add(PANEL);
 
         frame.setVisible(true);
+
+        Thread thread = new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            PANEL.repaint();
+        }
     }
 }
--- src/WelcomePanel.java
+++ src/WelcomePanel.java
@@ -1,9 +1,34 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-public class WelcomePanel {
-    private final JFrame FRAME;
+public class WelcomePanel extends JPanel implements ActionListener {
+    private BufferedImage BACKGROUND;
+    private JFrame FRAME;
+    private int yCoord;
 
     public WelcomePanel(JFrame frame) {
         FRAME = frame;
+        try {
+            BACKGROUND = ImageIO.read(new File("src/Images/Background.png"));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        yCoord = 0;
+    }
+
+    public void paintComponent(Graphics g) {
+        g.drawImage(BACKGROUND, 0, yCoord, null);
+        yCoord += 2;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
