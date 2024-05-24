faaa1b1d8bf732b0333e7389c32aaa7b34696383
Jonathan <jdonalds100@gmail.com>
2024-05-24 14:02:53 -0400

    2:02 PM

--- src/WelcomePanel.java
+++ src/WelcomePanel.java
@@ -3,37 +3,67 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class WelcomePanel extends JPanel implements ActionListener {
-    private BufferedImage BACKGROUND;
+    private Image BACKGROUND;
+    private Image JAVAMETRY_DASH;
+    private Image JONATHAN;
+    private Image RECTANGLE;
     private final JFrame FRAME;
-    private int xCoord;
+    private int jDashXCoord;
+    private int jonathanXCoord;
 
     public WelcomePanel(JFrame frame) {
         FRAME = frame;
         try {
-            BACKGROUND = ImageIO.read(new File("src/Images/Background.png"));
+            BACKGROUND = ImageIO.read(new File("src/Images/Javametry Dash.png"));
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-        xCoord = 0;
+        try {
+            JONATHAN = ImageIO.read(new File("src/Images/Jonathan.png")).getScaledInstance(200, 200, Image.SCALE_DEFAULT);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            JAVAMETRY_DASH = ImageIO.read(new File("src/Images/JavametryDash.png"));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            RECTANGLE = ImageIO.read(new File("src/Images/Rectangle.png"));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        jonathanXCoord = 0;
+        jDashXCoord = 5000;
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(BACKGROUND, xCoord, 0, null);
-        xCoord--;
+        g.drawImage(BACKGROUND, jonathanXCoord, 0, null);
+        jonathanXCoord--;
         try {
             Thread.sleep(2);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        if (xCoord == -200) {
+        if (jonathanXCoord == -200) {
             g.drawImage(BACKGROUND, 2560, 0, null);
-            xCoord = 0;
+            jonathanXCoord = 0;
+        }
+        g.drawImage(JONATHAN, 50, 500, null);
+        g.drawImage(JAVAMETRY_DASH, jDashXCoord, 20, null);
+        g.drawImage(RECTANGLE, -1000, 430, null);
+        if (jDashXCoord > 400) {
+            jDashXCoord--;
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
